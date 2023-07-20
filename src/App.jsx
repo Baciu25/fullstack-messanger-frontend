e23a319dc91df370fa0c3b428ebbbbf7d6254b48
baciu.alex <mircea.alexandru.baciu@students.beaminstitute.org>
2023-07-20 13:48:07 +0200

    emv

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,168 @@ import "./App.css";
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const [usernameInput, setUsernameInput] = useState("");
+  const [contentInput, setContentInput] = useState("");
+  const [editingId, setEditingId] = useState(null);
+  const [temporaryEditingContent, setTemporaryEditingContent] = useState("");
+  const [error, setError] = useState(null);
+
+  // WHEN THE APP LOADS, GET ALL MESSAGES
   useEffect(() => {
-    fetch("http://localhost:2510/messages")
-      .then((res) => res.json())
-      .then((data) => setMessages(data));
+    // GET REQUEST (POLLING EVERY 1 SECOND)
+    setInterval(() => {
+      fetch(`${import.meta.env.VITE_MESSAGING_API}/messages`, {
+        method: "GET",
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(
+              `Something went wrong with polling messages request${res.status}${res.statusText}`
+            );
+          }
+          return res.json();
+        })
+
+        .then((data) => setMessages(data))
+        .catch((error) => setError(error.message));
+    }, 1000);
   }, []);
-  useEffect(() => {
-    console.log(messages);
-  }, [messages]);
+
+  // CREATE MESSAGE
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const message = {
+      content: contentInput,
+      username: usernameInput,
+    };
+
+    fetch("http://localhost:2510/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(
+            `Something went wrong with creating messages request${res.status}${res.statusText}`
+          );
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("newDoc received from server", data);
+        setMessages([...messages, data]);
+      })
+      .catch((error) => setError(error.message));
+  };
+
+  // DELETE MESSAGE
+  const handleDelete = (id) => {
+    fetch(`${import.meta.env.VITE_MESSAGING_API}/messages/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok && response.status === 204) {
+        setMessages(messages.filter((message) => message.id !== id));
+      } else {
+        setError(
+          `Something went wrong with deleting ${response.status} ${response.statusText}`
+        );
+      }
+    });
+  };
+
+  const startOrFinishEditing = (id) => {
+    if (editingId === id) {
+      finishEditingAndSaveChangesInServer();
+    } else {
+      startEditing(id);
+    }
+  };
+
+  const startEditing = (id) => {
+    setEditingId((previous) => (previous === id ? null : id));
+    setTemporaryEditingContent(
+      messages.find((message) => message.id === id).content
+    );
+  };
+
+  const finishEditingAndSaveChangesInServer = () => {
+    setEditingId(null);
+    fetch(`${import.meta.env.VITE_MESSAGING_API}/messages/${editingId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: temporaryEditingContent }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(
+            `Something went wrong with editing messages request${res.status}${res.statusText}`
+          );
+        }
+        return res.json();
+      })
+
+      .then((updatedMessageFromBackend) => {
+        setMessages(
+          messages.map((message) =>
+            message.id === editingId ? updatedMessageFromBackend : message
+          )
+        );
+      });
+  };
 
   return (
     <>
+      {error && <h2> {error} </h2>}
+      <h1>editingId: {editingId ? editingId : "null"}</h1>
+
       {messages
-        .sort((a, b) => a.created - b.created)
+        .sort((a, b) => a.created_at - b.created_at)
         .map((message) => {
           return (
             <div key={message.id}>
-              {message.created}
-              {message.username} says {message.text}
+              {message.username}:{" "}
+              {editingId === message.id ? (
+                <input
+                  value={temporaryEditingContent}
+                  onChange={(e) => setTemporaryEditingContent(e.target.value)}
+                  name="content"
+                  type="text"
+                />
+              ) : (
+                message.content
+              )}
+              {new Date(message.created_at).toString()}
+              <button onClick={() => handleDelete(message.id)}>
+                Delete 🚮
+              </button>
+              <button onClick={() => startOrFinishEditing(message.id)}>
+                Update
+              </button>
             </div>
           );
         })}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          value={usernameInput}
+          onChange={(e) => setUsernameInput(e.target.value)}
+          name="username"
+          type="text"
+        />
+        <input
+          value={contentInput}
+          onChange={(e) => setContentInput(e.target.value)}
+          name="content"
+          type="text"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
